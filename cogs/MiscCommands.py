a4cf3443ee9d21bf0c108eaed501fa8e3959c2e4
Davi Oliveira <davicicfaculdade@gmail.com>
2021-01-25 09:34:14 -0300

    - Comando 'falar' adicionado.

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -2,15 +2,22 @@ import discord
 from discord.ext import commands
 
 
-class MiscCommands(commands.Cog):
+class MiscCommands(commands.Cog, name='Diversos'):
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='falar', aliases=['diga', 'say'])
-    async def falar(self, ctx):
-        await ctx.send('Teste')
-        print(ctx.message)
+    @commands.command(name='falar', aliases=['diga', 'say', 'fala'])
+    @commands.guild_only()
+    async def falar(self, ctx, channel: discord.TextChannel, *, arg):
+        await channel.send(arg)
+
+    @falar.error
+    async def falar_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("Não pude encontrar esse canal.")
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Você precisa mandar o canal e a mensagem a ser dita.")
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -7,16 +7,6 @@ from discord.ext import commands
 client = commands.Bot(command_prefix=config.prefix)
 
 
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
-
-
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-
-
 @client.event
 async def on_ready():
     print('Logado como {0.user}.'.format(client))
